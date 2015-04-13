f0e5f381c9ccf255f2d711f3248026e608f1fcbc
Ryan Anderson <ryan.anderson@buzzuti.com>
2015-04-13 14:39:54 -0500

    init ci

---
+++ src/UI/Services/PackageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetCompare.UI
+{
+    public interface IPackageService
+    {
+        
+    }
+    public class PackageService : IPackageService
+    {
+    }
+}
